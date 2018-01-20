fae6ce4fec24a340987bf5fa8289afca222b7a11
moguno <sns@0kn.sakura.ne.jp>
2018-01-21 08:43:54 +0900

    mikutter 3.5.9以降でしか動かないようにした。

--- mikutter-okaeri.rb
+++ mikutter-okaeri.rb
@@ -2,8 +2,14 @@
 
 Plugin.create(:mikutter_okaeri) {
   # systemユーザのアイコンを@mikutter_botのに差し替える
-  User.system.instance_eval {
-    self[:profile_image_url] = "http://dev.mikutter.hachune.net/projects/mikutter/repository/revisions/master/raw/core/skin/data/icon.png"
+  User.system.class.class_eval {
+    def icon
+      Enumerator.new{ |y|
+        Plugin.filtering(:photo_filter, "http://dev.mikutter.hachune.net/projects/mikutter/repository/revisions/master/raw/core/skin/data/icon.png", y)
+      }.map{|photo|
+        Plugin.filtering(:miracle_icon_filter, photo)[0]
+      }.first
+    end
   }
 
   # 感動の再会
